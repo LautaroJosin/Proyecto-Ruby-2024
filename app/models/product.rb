7dc5dd0ae4774fabdb3e4fa3613f6432f8c31623
Lautaro <ljosinsaller@gmail.com>
2024-12-09 20:02:05 -0300

    Agrego primeras validaciones al modelo product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
     has_many_attached :images
+
     belongs_to :category
 
     enum size: {
@@ -7,4 +8,21 @@ class Product < ApplicationRecord
         M: 'm',
         L: 'l', 
     }
+
+    validates: :title , presence: true
+    validates: :price , presence: true
+    validates: :stock , presence: true
+    validates :category, presence: true
+
+    validates: :size , presence: false
+    validates: :deleted_at , presence: false
+    validate :must_have_at_least_one_image
+
+    private
+    def must_have_at_least_one_image
+        if images.empty?
+        errors.add(:images, 'must have at least one image')
+        end
+    end
+    
 end
