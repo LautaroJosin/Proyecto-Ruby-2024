aff4e7f8a82387f785b6499432ab55294cf1d0d6
Lautaro <ljosinsaller@gmail.com>
2024-12-09 18:53:50 -0300

    Modifico products_controller, vista index de products y creo seeds basicos de products

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+10.times do
+    Product.create(
+      name: Faker::Commerce.product_name,
+      description: Faker::Lorem.paragraph,
+      price: Faker::Commerce.price(range: 10..100.0),
+      stock: rand(1..50),
+    )
+end
