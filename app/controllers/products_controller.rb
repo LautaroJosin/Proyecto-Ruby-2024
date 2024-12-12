cc3f512524741c33bba9f52bba44265e897b78ab
Lautaro <ljosinsaller@gmail.com>
2024-12-12 10:34:09 -0300

    Agrego controller Pages y agrego vista home

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,9 +6,6 @@ class ProductsController < ApplicationController
 
     def show
         @product = Product.find(params[:id])
-        if @product.nil?
-            redirect_to products_path, notice: 'Product not found', status: :not_found
-        end
     end
 
     def new
