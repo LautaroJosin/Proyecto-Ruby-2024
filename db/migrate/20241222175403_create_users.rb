e134c91a2fad288eee63bb37851a5fc8784eae36
Lautaro <ljosinsaller@gmail.com>
2024-12-22 15:02:25 -0300

    Creo modelos user y role

---
+++ db/migrate/20241222175403_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false, unique: true
+      t.string :email, null: false, unique: true
+      t.string :phone
+      t.string :password_digest, null: false
+      t.references :role, null: false, foreign_key: true # foreign key
+      t.date :join_date, null: false
+
+      t.timestamps
+    end
+
+    add_index :users, :name, unique: true
+    add_index :users, :email, unique: true
+  end
+end
