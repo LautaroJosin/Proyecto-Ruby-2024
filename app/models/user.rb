92aaecc2974ded641ce930dd79a26a128d5c2f2c
Lautaro <ljosinsaller@gmail.com>
2024-12-24 12:42:23 -0300

    Modifico modelo user y agrego gema bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   belongs_to :role
+
+  has_secure_password # Requiere la gema bcrypt
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone, presence: true
+  validates :join_date, presence: true
 end
