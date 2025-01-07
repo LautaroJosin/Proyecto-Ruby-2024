5148a97d62d2a440fbb10fe4478a177ed5ca72cf
Lautaro <ljosinsaller@gmail.com>
2025-01-06 21:30:18 -0300

    Creo controlador Users, mejoro el modelo user, agrego el boton log in y log out al header y modifico las rutas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+  enum :role_int, admin: 0, manager: 1, employee: 2
+  # before_create :set_joined_at
+  # before_update :check_if_active_changed
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Formato de email incorrecto." }
+  validates :phone, presence: true, format: { with: /\A\d{6,10}\z/, message: "El telefono debe tener entre 6 y 10 numeros" }
+  validates :role_int, inclusion: { in: role_ints.keys.map { |role| role.to_s }, message: "%{value} Role invalido." }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions" }, path_names: { sign_in: "login", sign_out: "logout" }
   resources :categories
   resources :products
 
