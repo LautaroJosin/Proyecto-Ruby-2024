bcb9c8095f009c26342d89a596acf22a7bec614e
Lautaro <ljosinsaller@gmail.com>
2025-01-14 19:37:58 -0300

    Hago cambios en metodo create sales y agrego validaciones en vista index de sales

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -7,7 +7,7 @@ class SalesController < ApplicationController
     @product_amount = params[:products_amount].to_i
 
     if @product_amount <= 0 || @product_amount.nil?
-      flash[:alert] = "Please select the number of products you want to include into the sale"
+      flash.now[:alert] = "Please select the number of products you want to include into the sale"
       render :set_products_amount
     else
       @sale = Sale.new
@@ -15,47 +15,39 @@ class SalesController < ApplicationController
     end
   end
 
+  # This method is used to create a new sale in the following way:
+  # 1. The sale will be created
+  # 2. The stock of the products included in the sale will be updated
+  # If any of the steps fails, the transaction will be rolled back
   def create
     ActiveRecord::Base.transaction do
-      # Verificar si el cliente es nuevo o ya existe. Eliminar los parámetros de cliente si no se está creando uno nuevo
-      if params[:sale][:client_id].blank?
-        @client_params = params[:sale].slice(:name, :lastname, :dni, :phone)
-        @client = Client.create!(@client_params)
-        params[:sale][:client_id] = @client.id
-        logger.info "New client created: #{@client.inspect}"
-      else
-        logger.info "Existing client selected: #{params[:sale][:client_id]}"
-      end
-
-      params[:sale].delete(:name)
-      params[:sale].delete(:lastname)
-      params[:sale].delete(:dni)
-      params[:sale].delete(:phone)
-
-
-      # Create sale
       @sale = Sale.new(sale_params)
 
-      # Calcular el total y actualizar la venta
+      if @sale.invalid?
+        flash.now[:alert] = @sale.errors.full_messages.join(" | ")
+        render :new
+        return
+      end
+
       total = @sale.product_sale.map { |ps| ps.product.price * ps.quantity }.sum
       @sale.total_price = total
-      logger.info "Total calculated: #{@sale.total_price}"
 
-      # Save the sale in the bd and updates the product's stock
       if @sale.save
+
         @sale.product_sale.each do |ps|
           product = ps.product
           product.update!(stock: product.stock - ps.quantity)
-          redirect_to sales_path, notice: "Sale saved correctly"
         end
+        redirect_to sales_path, notice: "Sale saved correctly"
+        return
       else
-        pp @sale.errors
-        # flash.now[:alert] = "An error has ocurred when saving the sale"
         raise ActiveRecord::Rollback
       end
     end
-  end
 
+    flash.now[:alert] = "There was an error saving the sale: #{@sale.errors.full_messages.join(" | ")}"
+    render :new
+  end
 
   # This method is used to set the number of products that will be included into the sale
   def set_products_amount
@@ -66,7 +58,7 @@ class SalesController < ApplicationController
       @products_amount = params[:products_amount].to_i
 
       if @products_amount <= 0
-        flash[:alert] = "Please select a number greater than 0"
+        flash.now[:alert] = "Please select a number greater than 0"
         render :set_products_amount
       else
         redirect_to new_sale_path(products_amount: @products_amount)
@@ -74,8 +66,23 @@ class SalesController < ApplicationController
     end
   end
 
+  def cancel
+    @sale = Sale.find(params[:id])
+    if @sale.active == false
+      return redirect_to sales_path, notice: "The sale is already canceled"
+    end
+    ActiveRecord::Base.transaction do
+      @sale.update!(active: false)
+      @sale.product_sale.each do |ps|
+        product = ps.product
+        product.update!(stock: product.stock + ps.quantity)
+      end
+    end
+    redirect_to sales_path, notice: "Sale canceled and stock reverted"
+  end
+
   private
   def sale_params
-    params.require(:sale).permit(:user_id, :client_id, :name, :lastname, :dni, :phone, product_sale_attributes: [  :product_id, :quantity, :_destroy ])
+    params.require(:sale).permit(:user_id, :client_id, product_sale_attributes: [  :product_id, :quantity, :_destroy ])
   end
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -4,18 +4,24 @@ class Sale < ApplicationRecord
 
   belongs_to :user
   belongs_to :client
-
   has_many :product_sale, dependent: :destroy
   has_many :product, through: :product_sale
 
   accepts_nested_attributes_for :product_sale, allow_destroy: true
 
-  validates :user_id, :client_id, presence: true
-  validates :total_price, presence: true, numericality: { greater_than: 0 }
+  validates :user_id, presence: true
+  validates :client_id, presence: true
 
   private
 
   def check_stock
+    product_sale.each do |ps|
+      product = ps.product
+      if product.stock < ps.quantity
+        errors.add(:base, "The product #{product.name} does not have enough stock")
+        throw :abort
+      end
+    end
   end
 
   def set_date_and_time
