52a5fa7aeb74efad8533d7dbe7287cd0cfde62b1
Lautaro <ljosinsaller@gmail.com>
2025-01-15 12:27:00 -0300

    Optimizo la carga de resultados en pagina home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @products = Product.includes(:category, images_attachments: :blob)
+
     if params[:category_id].present?
-      @products = Product.where(category_id: params[:category_id])
-    else
-      @products = Product.all
+      @products = @products.where(category_id: params[:category_id])
     end
 
     if params[:search].present?
-      @products = @products.where("LOWER(name) LIKE ? OR LOWER(description) LIKE ?", "%#{params[:search].downcase}%", "%#{params[:search].downcase}%")
+      search_query = "%#{params[:search].downcase}%"
+      @products = @products.where("LOWER(name) LIKE ? OR LOWER(description) LIKE ?", search_query, search_query)
     end
 
     @products = @products.where("stock > ?", 0)
